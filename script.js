0529de656d4cfd8e8f99bbeef4da29bb3325a2ef
Abel Orsolya-Szekely <104093355+oszabel@users.noreply.github.com>
2023-02-06 10:12:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let logo = document.querySelector('.container');
+let menu = document.querySelector('.menu');
+
+logo.addEventListener('click',function(){
+    menu.classList.toggle('showmenu');
+});
