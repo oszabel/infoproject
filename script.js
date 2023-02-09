8b2e8a85a13c88b4e117c008b5e6140f9196428e
Abel Orsolya-Szekely <104093355+oszabel@users.noreply.github.com>
2023-02-09 12:18:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+let body = document.querySelector('body');
+try{
+    body.classList=localStorage.getItem("light");
+} catch (e) {
+    console.log(e)
+}
+
 let logo = document.querySelector('.container');
 let menu = document.querySelector('.menu');
 
 logo.addEventListener('click',function(){
     menu.classList.toggle('showmenu');
+});
+
+let light = document.querySelector('.light');
+
+light.addEventListener('click',function(){
+    body.classList.toggle('lightmode');
+    // document.getElementById('light').innerHTML = 'Sötét mód';
+    localStorage.setItem("light",body.classList);
+    document.querySelector('.light').innerHTML = body.classList.contains('lightmode')? 'Sötét mód':'Világos mód';
 });
