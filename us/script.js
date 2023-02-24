90013d4c71cca5ce57e30b49ed67315cc690a27f
Abel Orsolya-Szekely <104093355+oszabel@users.noreply.github.com>
2023-02-24 11:15:06 +0100

    Add files via upload

---
+++ us/script.js
@@ -0,0 +1,30 @@
+let body = document.querySelector('body');
+try{
+    body.classList=localStorage.getItem("light");
+} catch (e) {
+    console.log(e)
+}
+
+let logo = document.querySelector('.container');
+let menu = document.querySelector('.menu');
+
+logo.addEventListener('click',function(){
+    menu.classList.toggle('showmenu');
+    logo.classList.toggle('animation');
+});
+
+let light = document.querySelector('.light');
+
+light.addEventListener('click',function(){
+    body.classList.toggle('lightmode');
+    // document.getElementById('light').innerHTML = 'Sötét mód';
+    localStorage.setItem("light",body.classList);
+    // document.querySelector('.light').innerHTML = body.classList.contains('lightmode')? 'Sötét mód':'Világos mód';
+    document.querySelector('.light').innerHTML = body.classList.contains('lightmode')? 'Dark mode':'Light mode';
+});
+
+let husvetitojas = document.querySelector('.husvetitojas');
+
+husvetitojas.addEventListener('click',function(){
+    window.alert('Szia Milán!');
+});
